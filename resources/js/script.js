ea32f2ccab8d8f7874321597fe60e15108d3aa2e
mhmdbehairy <mhmd.behairy@gmail.com>
2019-01-18 12:09:28 +0200

    Add Anmations with jQuery

--- resources/js/script.js
+++ resources/js/script.js
@@ -4,7 +4,8 @@
 
 $('document').ready(function() { 
 /* Ensures that the code can only run after the document has been loaded */
-                  
+    
+    /* STICKY NAVIGATION */
     $('.js--section-features').waypoint(function(direction) {
         if (direction == "down") {
             $('nav').addClass('sticky');
@@ -14,6 +15,31 @@ $('document').ready(function() {
         }
     }, {
         offset: '60px;'
+    });
+    
+    /* ANIMATIONS ON SCROLL */
+    $('.js--wp-1').waypoint(function(direction) {
+        $('.js--wp-1').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+    
+    $('.js--wp-2').waypoint(function(direction) {
+        $('.js--wp-2').addClass('animated fadeInUp');
+    }, {
+        offset: '50%'
+    });
+    
+    $('.js--wp-3').waypoint(function(direction) {
+        $('.js--wp-3').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+    
+    $('.js--wp-4').waypoint(function(direction) {
+        $('.js--wp-4').addClass('animated pulse');
+    }, {
+        offset: '50%'
     });
 
 });
